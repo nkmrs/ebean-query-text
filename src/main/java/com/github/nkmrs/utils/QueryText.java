78a58d4da30867e7574596b855be13070a0c0270
s_nakamura <s_nakamura@densanjoho.co.jp>
2017-01-25 14:25:37 +0900

    refactor and don’t static, changed API

--- src/main/java/com/github/nkmrs/utils/QueryText.java
+++ src/main/java/com/github/nkmrs/utils/QueryText.java
@@ -1,5 +1,7 @@
 package com.github.nkmrs.utils;
 
+import com.avaje.ebean.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,11 +10,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.avaje.ebean.Ebean;
-import com.avaje.ebean.Query;
-import com.avaje.ebean.RawSql;
-import com.avaje.ebean.RawSqlBuilder;
-
 /**
  * Load query text for Ebean.
  *
@@ -20,93 +17,106 @@ import com.avaje.ebean.RawSqlBuilder;
  */
 public class QueryText {
 
-	public static final String QUERY_DIR = "conf/queries/";
-	public static final String QUERY_EXT = ".sql";
-	public static final String ENCODING = "UTF-8";
+    private static final String DEFAULT_QUERY_DIR = "conf/queries/";
+    private static final String DEFAULT_QUERY_EXT = ".sql";
 
-	/**
-	 * SQL query cache
-	 */
-	private static Map<String, String> queries = loadQueries();
+    /**
+     * SQL query cache
+     */
+    private final Map<String, String> queries;
 
-	private QueryText() {}
+    private final EbeanServer server;
 
-	private static Map<String, String> loadQueries() {
-		Map<String, String> map = new HashMap<>();
+    private QueryText(EbeanServer server, String queryDir, String queryExt) {
+        this.server = server;
+        this.queries = loadQueries(queryDir, queryExt);
+    }
 
-		File rootdir = new File(QUERY_DIR);
-		for (File f : rootdir.listFiles((dir, name) -> (name.endsWith(QUERY_EXT)))) {
-			String name = f.getName()
-					.replaceAll(QUERY_EXT + "$", "");
-			String queryText = loadText(f.toPath())
-					.replaceAll("^SELECT", "select ");
-			map.put(name, queryText);
-		}
+    /**
+     * getSqlQueryText new instance by default server
+     *
+     * @return new instance by default server
+     */
+    public static QueryText getDefault() {
+        return new QueryText(Ebean.getDefaultServer(), DEFAULT_QUERY_DIR, DEFAULT_QUERY_EXT);
+    }
 
-		return map;
-	}
+    private static Map<String, String> loadQueries(String basedir, String ext) {
+        Map<String, String> map = new HashMap<>();
 
-	private static String loadText(Path path) {
-		try {
-			return Files.readAllLines(path).stream()
-					.collect(Collectors.joining(" "));
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "";
-		}
-	}
+        File rootdir = new File(basedir);
+        for (File f : rootdir.listFiles((dir, name) -> (name.endsWith(ext)))) {
+            String name = f.getName()
+                    .replaceAll(ext + "$", "");
+            String queryText = loadText(f.toPath())
+                    .replaceAll("^SELECT", "select ");
+            map.put(name, queryText);
+        }
 
-	/**
-	 * get SQL query text by query file name.
-	 *
-	 * @param key query filename (without extension)
-	 * @return SQL query text
-	 */
-	private static String get(String key) {
-		if (!queries.containsKey(key)) {
-			throw new IllegalArgumentException("Query text not found: " + key);
-		}
-		return queries.get(key);
-	}
+        return map;
+    }
 
-	/**
-	 * building Query object by query file name.
-	 *
-	 * @param key query filename (without extension)
-	 * @param clazz type of Query result
-	 * @param <T> type of Query result
-	 * @return SQL query text
-	 */
-	public static <T> Query<T> getQuery(String key, Class<T> clazz) {
-		return getQuery(key, clazz, "");
-	}
+    private static String loadText(Path path) {
+        try {
+            return Files.readAllLines(path).stream()
+                    .collect(Collectors.joining(" "));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
 
-	/**
-	 * building Query object by query file name.
-	 *
-	 * @param key query filename (without extension)
-	 * @param clazz type of Query result
-	 * @param <T> type of Query result
-	 * @param orderByString Order by string
-	 * @return SQL query text
-	 */
-	public static <T> Query<T> getQuery(String key, Class<T> clazz,
-			String orderByString) {
-		String sql = get(key) + " " + orderByString;
-		return getQueryBySql(sql, clazz);
-	}
+    /**
+     * getSqlQueryText SQL query text by query file name.
+     *
+     * @param key query filename (without extension)
+     * @return SQL query text
+     */
+    private String getSqlQueryText(String key) {
+        if (!queries.containsKey(key)) {
+            throw new IllegalArgumentException("Query text not found: " + key);
+        }
+        return queries.get(key);
+    }
 
-	/**
-	 * building Query object by another SQL text.
-	 *
-	 * @param sql SQL query text
-	 * @param clazz type of Query result
-	 * @param <T> type of Query result
-	 * @return Query object
-	 */
-	public static <T> Query<T> getQueryBySql(String sql, Class<T> clazz) {
-		RawSql rawSql = RawSqlBuilder.parse(sql).create();
-		Query<T> query = Ebean.find(clazz).setRawSql(rawSql);
-		return query;
-	}
+    /**
+     * building Query object by query file name.
+     *
+     * @param key   query filename (without extension)
+     * @param clazz type of Query result
+     * @param <T>   type of Query result
+     * @return SQL query text
+     */
+    public <T> Query<T> getQuery(String key, Class<T> clazz) {
+        return getQuery(key, clazz, "");
+    }
+
+    /**
+     * building Query object by query file name.
+     *
+     * @param key           query filename (without extension)
+     * @param clazz         type of Query result
+     * @param <T>           type of Query result
+     * @param orderByString Order by string
+     * @return SQL query text
+     */
+    public <T> Query<T> getQuery(String key, Class<T> clazz,
+                                 String orderByString) {
+        String sql = getSqlQueryText(key) + " " + orderByString;
+        return getQueryBySql(sql, clazz);
+    }
+
+    /**
+     * building Query object by another SQL text.
+     *
+     * @param sql   SQL query text
+     * @param clazz type of Query result
+     * @param <T>   type of Query result
+     * @return Query object
+     */
+    public <T> Query<T> getQueryBySql(String sql, Class<T> clazz) {
+        RawSql rawSql = RawSqlBuilder.parse(sql).create();
+        Query<T> query = server.find(clazz).setRawSql(rawSql);
+        return query;
+    }
 }
--- src/test/java/com/github/nkmrs/utils/QueryTextTest.java
+++ src/test/java/com/github/nkmrs/utils/QueryTextTest.java
@@ -1,73 +1,71 @@
 package com.github.nkmrs.utils;
 
-import static org.junit.Assert.*;
-
-import java.util.List;
-
+import com.avaje.ebean.Query;
+import models.Example1;
+import models.UserAge;
+import models.UserName;
 import org.avaje.agentloader.AgentLoader;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.avaje.ebean.Query;
+import java.util.List;
 
-import models.Example1;
-import models.UserAge;
-import models.UserName;
+import static org.junit.Assert.assertEquals;
 
 public class QueryTextTest {
 
-	@BeforeClass
-	public static void setup() {
-		AgentLoader.loadAgentFromClasspath("ebean-agent", "");
+    @BeforeClass
+    public static void setup() {
+        AgentLoader.loadAgentFromClasspath("ebean-agent", "");
 
-		createUserName("Johannes"); // id = 1
-		createUserName("Riko"); // id = 2
-		createUserName("Marry"); // id = 3
+        createUserName("Johannes"); // id = 1
+        createUserName("Riko"); // id = 2
+        createUserName("Marry"); // id = 3
 
-		createUserAge(15); // id = 1
-		createUserAge(16); // id = 2
-		createUserAge(17); // id = 3
-	}
+        createUserAge(15); // id = 1
+        createUserAge(16); // id = 2
+        createUserAge(17); // id = 3
+    }
 
-	private static void createUserName(String name) {
-		UserName n = new UserName();
-		n.name = name;
-		n.save();
-	}
+    private static void createUserName(String name) {
+        UserName n = new UserName();
+        n.name = name;
+        n.save();
+    }
 
-	private static void createUserAge(Integer name) {
-		UserAge n = new UserAge();
-		n.age = name;
-		n.save();
-	}
+    private static void createUserAge(Integer name) {
+        UserAge n = new UserAge();
+        n.age = name;
+        n.save();
+    }
 
-	@Test
-	public void findAll() {
-		Query<Example1> q = QueryText.getQuery("example1", Example1.class);
-		List<Example1> list = q.findList();
-		assertEquals(list.size(), 3);
-	}
+    @Test
+    public void findAll() {
+        Query<Example1> q = QueryText.getDefault().getQuery("example1", Example1.class);
+        List<Example1> list = q.findList();
+        assertEquals(list.size(), 3);
+    }
 
-	@Test
-	public void order() {
-		String orderByString = "order by name desc";
-		Query<Example1> q = QueryText.getQuery("example1", Example1.class, orderByString);
-		List<Example1> list = q.findList();
-		assertEquals((int) list.get(0).id, 2);
-		assertEquals((int) list.get(1).id, 3);
-		assertEquals((int) list.get(2).id, 1);
-	}
+    @Test
+    public void order() {
+        String orderByString = "order by name desc";
+        Query<Example1> q = QueryText.getDefault().getQuery("example1", Example1.class, orderByString);
+        List<Example1> list = q.findList();
+        assertEquals((int) list.get(0).id, 2);
+        assertEquals((int) list.get(1).id, 3);
+        assertEquals((int) list.get(2).id, 1);
+    }
 
-	@Test
-	public void where() {
-		String orderByString = "order by name";
-		Query<Example1> q = QueryText.getQuery("example1", Example1.class, orderByString);
-		List<Example1> list = q.where()
-				.ge("age", 16)
-				.findList();
-		assertEquals(list.size(), 2);
-		assertEquals((int) list.get(0).id, 3);
-		assertEquals((int) list.get(1).id, 2);
-	}
+    @Test
+    public void where() {
+        String orderByString = "order by name";
+        Query<Example1> q = QueryText.getDefault().getQuery("example1", Example1.class, orderByString);
+        List<Example1> list = q.where()
+                .ge("age", 16)
+                .findList();
+        assertEquals(list.size(), 2);
+        assertEquals((int) list.get(0).id, 3);
+        assertEquals((int) list.get(1).id, 2);
+    }
 
 }
